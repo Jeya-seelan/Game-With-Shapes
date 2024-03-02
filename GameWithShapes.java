815a19767ef8c8e44c63cc7cdb1a6d703cfe28ab
Jeyaseelan I <108184551+Jeya-seelan@users.noreply.github.com>
2024-03-02 13:44:35 +0530

    Add files via upload

---
+++ GameWithShapes.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class GameWithShapes{
+ public static void main(String args[]){
+  Scanner in = new Scanner(System.in);
+  int l = in.nextInt();
+  int r = in.nextInt();
+  
+  int s = l*l;
+  int c = (22/7)*r*r;
+  
+  if(s>=c)
+   System.out.println("Circle can be inside a Square");
+  else
+   System.out.println("Circle cannot be inside a Square");
+ } 
+}
